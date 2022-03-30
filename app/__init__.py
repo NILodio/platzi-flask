05d1672432a5b4b8ec6a5ed2039fc78cb53c0bd2
Danilo Diaz <ddiazva13@gmail.com>
2022-03-30 16:40:38 -0500

    @@

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
+from .auth import auth
 from .config import Config
 
 
@@ -9,5 +10,7 @@ def create_app():
     bootstrap = Bootstrap(app)
 
     app.config.from_object(Config)
+
+    app.register_blueprint(auth)
 
     return app
